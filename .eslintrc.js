d6c383c9a2ae864391ed40ebefe38c701467d229
Runzhou Li (Leo) <woozyking@users.noreply.github.com>
2018-10-14 23:09:54 -0400

    remove redundant eslint comma-dangle rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,10 +25,6 @@ module.exports = {
       "error",
       "never",
     ],
-    "comma-dangle": [
-      "error",
-      "never",
-    ],
     "no-console": [
       "error",
       { allow: ["warn", "error"] },
