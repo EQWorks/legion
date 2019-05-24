aa16009ec233749b766d8954a120b2151e36488e
woozyking <runzhou.li@gmail.com>
2019-05-24 14:35:51 -0400

    Revise /diff response with block kit based rich message

--- modules/diff.js
+++ modules/diff.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const axios = require('axios')
-const { WebClient } = require('@slack/web-api')
 
 const { verifySlack } = require('./middleware')
 
-const { GITHUB_TOKEN, SLACK_BOT_TOKEN } = process.env
+const { GITHUB_TOKEN } = process.env
 
 
 // TODO: only supports 2-stage comparison for now
@@ -30,10 +29,7 @@ const router = express.Router()
 
 
 router.all('/', verifySlack, async (req, res, next) => {
-  const {
-    text: product = 'firstorder',
-    channel_id: channel = 'C1FSD8B2T',
-  } = req.body || {}
+  const { text: product = 'firstorder' } = req.body || {}
   const { baseURL = '', stages = [], key } = PRODUCTS[product] || {}
 
   if (!baseURL) {
@@ -42,42 +38,98 @@ router.all('/', verifySlack, async (req, res, next) => {
     return next(error)
   }
 
-  const [ dev, prod ] = stages
+  const [dev, prod] = stages
 
   try {
-    const responses = await Promise.all(
-      stages.map((stage) => axios.get(`/${stage}`, { baseURL })))
-    const [ head, base ] = responses.map(({ data = {} }) => data[key])
-
-    const web = new WebClient(SLACK_BOT_TOKEN)
-    const texts = []
-
-    if (base === head) {
-      texts.push(`*${product}* \`${dev}\` and \`${prod}\` are on the same \`${key}\` (\`${base}\`)`)
-    } else {
-      const { data: { commits } } = await axios.get(
-        `/repos/EQWorks/${product}/compare/${base}...${head}`,
+    const responses = await Promise.all(stages.map((stage) => axios.get(`/${stage}`, { baseURL })))
+    const [head, base] = responses.map(({ data = {} }) => data[key])
+    const cxtBlk = {
+      type: 'context',
+      elements: [
         {
-          baseURL: 'https://api.github.com',
-          auth: {
-            username: 'woozyking',
-            password: GITHUB_TOKEN,
-          },
+          type: 'plain_text',
+          text: `$ git log --pretty=oneline ${base}...${head}`,
         }
-      )
-      const info = commits.map(({ sha, commit: { message } }) => ({ sha, message }))
-      info.reverse()
-
-      texts.push(`*${product}* \`${dev}\` and \`${prod}\` are not on the same \`${key}\``)
-      texts.push(`\`$ git log --pretty=oneline ${base}...${head}\``)
-      texts.push(`\`\`\`${JSON.stringify(info, null, 2)}\`\`\``)
+      ]
+    }
+    if (base === head) {
+      return res.status(200).json({
+        response_type: 'in_channel',
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `*${product}* \`${dev}\` and \`${prod}\` are on the same \`${key}\` (\`${base}\`)`,
+            }
+          },
+          { type: 'divider' },
+          cxtBlk,
+        ],
+      })
     }
 
-    await web.chat.postMessage({ channel, text: texts.join('\n') })
+    const { data: { commits } } = await axios.get(
+      `/repos/EQWorks/${product}/compare/${base}...${head}`,
+      {
+        baseURL: 'https://api.github.com',
+        auth: {
+          username: 'woozyking',
+          password: GITHUB_TOKEN,
+        },
+      }
+    )
+    const info = commits.map(({
+      sha,
+      html_url,
+      commit: { message },
+      author,
+      committer,
+    }) => ([
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: message,
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: sha.slice(0, 7),
+          },
+          url: html_url,
+          value: sha,
+        },
+      },
+      {
+        type: 'context',
+        elements: [
+          {
+            type: 'plain_text',
+            text: (author || committer || {}).login || 'Unknown Code Genie',
+          },
+        ]
+      },
+      { type: 'divider' },
+    ]))
+    info.reverse()
 
-    return res.json({
-      [dev]: head,
-      [prod]: base,
+    return res.status(200).json({
+      response_type: 'in_channel',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*${product}* \`${dev}\` and \`${prod}\` are on different \`${key}\``,
+          }
+        },
+        { type: 'divider' },
+        // TODO: Array.prototype.flat not avail in Node until v11+
+        ...[].concat.apply([], info),
+        cxtBlk,
+      ],
     })
   } catch(err) {
     return next(err)
