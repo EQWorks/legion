7ab7f6a33d1471e309adf4c0cf614a8aebae4e37
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-03-02 12:42:25 -0500

    worker - fix reference to worker functions

--- slack-worker.js
+++ slack-worker.js
@@ -2,7 +2,7 @@ const modules = require('./modules')
 
 // `type`` is the name of the export key in ./modules/index
 module.exports.handler = async ({ type, payload }) => {
-  const worker = modules(type)
+  const { worker } = modules[type]
   await worker(payload)
   return { statusCode: 200 }
 }
