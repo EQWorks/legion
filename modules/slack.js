b6a3a484ea12450f695b4a4aaaabbcc69b0bb2d9
Shane Stratton <srs_34@hotmail.com>
2020-03-15 17:39:58 -0400

    Slack - add highlight tags for more detailed summary of threads

--- modules/slack.js
+++ modules/slack.js
@@ -8,7 +8,28 @@ const token = process.env.SLACK_OAUTH
 const web = new WebClient(token)
 
 
-const worker = async ({ command, text, user_id, response_url, channel_name, channel_id }) => {
+const THREAD_HIGHLIGHTS = [
+  {
+    text: 'Summary',
+    tag: '?!summary',
+  },
+  {
+    text: 'TODOs',
+    tag: '?!TODO',
+  },
+  {
+    text: 'Questions',
+    tag: '?!question',
+  },
+  {
+    text: 'Links',
+    tag: '?!link'
+  },
+]
+
+const TAG_REPLACE = new RegExp(THREAD_HIGHLIGHTS.map(({ tag }) => `(${tag.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})`).join('|'), 'g')
+
+const worker = async ({ command, search, user_id, response_url, channel_name, channel_id }) => {
   if (command === 'threads') {
     let finalName = channel_name
     if (channel_name === 'privategroup') {
@@ -17,7 +38,7 @@ const worker = async ({ command, text, user_id, response_url, channel_name, chan
     }
     // TODO: `![tag]` should be passed in and this used as default!!
     const result = await web.search.messages({
-      query: `${text} in:${finalName} after:${new Date().toISOString().substring(0, 7)}-${new Date().getDate() - 1}`,
+      query: `${search} in:${finalName} after:${new Date().toISOString().substring(0, 7)}-${new Date().getDate() - 1}`,
     })
     // The result contains an identifier for the message, `ts`.
     // thread messages normally use thread_ts
@@ -25,7 +46,14 @@ const worker = async ({ command, text, user_id, response_url, channel_name, chan
     let threads = await Promise.all(result.messages.matches.map(async ({ text, permalink, ts }) => {
       const thread = await web.conversations.replies({ channel: channel_id, ts})
       if (thread.messages.length > 1) {
-        return { text, permalink, replies: thread.messages.length - 1 }
+        // search each message for highlight tags and return text
+        const highlights = THREAD_HIGHLIGHTS.map(({ tag, text }) => ({
+          text,
+          content: thread.messages
+            .filter(({ text }) => text.indexOf(tag) >= 0)
+            .map(({ text }) => text.replace(TAG_REPLACE, '')),
+        })).filter(({ content }) => content.length)
+        return { text, permalink, replies: thread.messages.length - 1, highlights }
       } else {
         return false
       }
@@ -38,14 +66,33 @@ const worker = async ({ command, text, user_id, response_url, channel_name, chan
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `${threads.length ? `${threads.map(({ text, replies, permalink }) => `\n • ${text.replace('!thread:', '')} - ${replies} msgs - <${permalink}| See Thread>`)}` : 'No Threads Found'}`
+            text: `${threads.length ? `Showing threads for *${search}*` : 'No threads found.'}`
           },
-        }
+        },
+        {
+          type: 'divider',
+        },
+        ...threads.map(({ text, replies, permalink, highlights }) => ([
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `*Thread:* ${text.replace(search, '')} - ${replies} msgs - <${permalink}| See Thread>`,
+            }
+          },
+          {
+            type: 'context',
+            elements: highlights.length ? highlights.map(({ text, content }) => ({
+              type: 'mrkdwn',
+              text: `*${text}* ${content.map(o => `\n• ${o.trim()}`).join('')}`
+            })) : [{ type: 'plain_text', text: 'No highlights' }],
+          },
+        ])).flat()
       ],
     })
   } else {
     const result = await web.search.messages({
-      query: `${text} after:${new Date().toISOString().substring(0, 7)}-${new Date().getDate() - 1}`,
+      query: `${search} after:${new Date().toISOString().substring(0, 7)}-${new Date().getDate() - 1}`,
     })
 
     return axios.post(response_url, {
@@ -55,7 +102,7 @@ const worker = async ({ command, text, user_id, response_url, channel_name, chan
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `${text === user_id ? 'You' : 'They'} were pinged ${result.messages.matches.length} time(s) today!`,
+            text: `${search === user_id ? 'You' : 'They'} were pinged ${result.messages.matches.length} time(s) today!`,
           },
         }
       ],
@@ -82,14 +129,16 @@ const route = (req, res) => {
       trigger_id: '957681200677.49467158547.bcd8bacc8087edc008d15d200bfdad49'
     }
   */
-  let finalText = text
+  let search = text
   if (command === 'pings') {
-    finalText = text === '' ? user_id : text.replace('<','').split('|')[0]
+    search = text === '' ? user_id : text.replace('<','').split('|')[0]
+  } else {
+    search = text
   }
   const payload = {
     command,
     user_id,
-    text: finalText,
+    search,
     channel_id,
     channel_name,
     response_url,
--- slack-worker.js
+++ slack-worker.js
@@ -1,6 +1,6 @@
 const modules = require('./modules')
 
-// `type`` is the name of the export key in ./modules/index
+// `type` is the name of the export key in ./modules/index
 module.exports.handler = async ({ type, payload }) => {
   const { worker } = modules[type]
   await worker(payload)
