96130935752c35806d5957eaa667cfe6f0750c78
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2021-12-23 09:48:23 -0500

    dev - add logging statements

--- slack-worker.js
+++ slack-worker.js
@@ -2,6 +2,9 @@ const modules = require('./modules')
 
 // `type` is the name of the export key in ./modules/index
 module.exports.handler = async ({ type, payload }) => {
+  console.log(`type ${type}`)
+  console.log(`payload ${payload}`)
+  console.dir(payload)
   const { worker } = modules[type]
   await worker(payload)
   return { statusCode: 200 }
