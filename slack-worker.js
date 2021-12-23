686028bf08d90e67f4c574112fa3b80e57c5bc61
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2021-12-23 10:12:14 -0500

    worker - correctly access module routes

--- slack-worker.js
+++ slack-worker.js
@@ -1,11 +1,8 @@
-const modules = require('./modules')
+const { routes } = require('./modules')
 
 // `type` is the name of the export key in ./modules/index
 module.exports.handler = async ({ type, payload }) => {
-  console.log(`type ${type}`)
-  console.log(`payload ${payload}`)
-  console.dir(payload)
-  const { worker } = modules[type]
+  const { worker } = routes[type]
   await worker(payload)
   return { statusCode: 200 }
 }
