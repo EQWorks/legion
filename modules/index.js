d3edbe1ec0e5721f519428dd640b8fd6530ea9fd
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2022-01-14 17:44:44 -0500

    commands - enable demo command pending viewHandler fix

--- modules/index.js
+++ modules/index.js
@@ -3,6 +3,6 @@ module.exports.commands = {
   release: require('./commands/release'),
   food: require('./commands/food'),
   pipeline: require('./commands/pipeline'),
-  // demo: require('./commands/demo'), // TODO: needs work on the view handler
+  demo: require('./commands/demo'), // TODO: needs work on the view handler
   // TODO: and migrate other ones from modules/need-revision
 }
