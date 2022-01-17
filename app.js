828955e9082ea42457be512c6ecd463083d8c779
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2022-01-17 11:58:12 -0500

    app - consolidate dynamic command/view mounting

--- app.js
+++ app.js
@@ -12,30 +12,28 @@ const app = new App({
   receiver,
   socketMode: false,
 })
-/* sample command object given to listener({ command, ack, ... })
-  {
-    token: 'm4Q8xY7jIYRbwM0W7u7utV30',
-    team_id: 'T1FDR4NG3',
-    team_domain: 'eqworks',
-    channel_id: 'CCCB6QD9S',
-    channel_name: 'bot-cmd', // <- this eliminates the need for getChannelName
-    user_id: 'U1FF6KSF9',
-    user_name: 'woozyking',
-    command: '/diff',
-    text: 'snoke',
-    api_app_id: 'A6HPM5VC0',
-    is_enterprise_install: 'false',
-    response_url: 'https://hooks.slack.com/commands/T1FDR4NG3/2956432472786/J8Q17JPFukiLurI8zbVJzTCX',
-    trigger_id: '2956462218819.49467158547.0dae80ec6d595b045efcf7a8fee079e7'
-  }
-*/
-Object.entries(commands).forEach(([name, { listener }]) => {
-  app.command(`/${name}`, listener)
-})
 
-// interactive views, by callback_id
-// TODO: ones that require rework are commented out
-Object.entries(commands).forEach(([name, { viewHandler }]) => {
+Object.entries(commands).forEach(([name, { listener, viewHandler }]) => {
+  /* sample command object given to listener({ command, ack, ... })
+    {
+      token: 'm4Q8xY7jIYRbwM0W7u7utV30',
+      team_id: 'T1FDR4NG3',
+      team_domain: 'eqworks',
+      channel_id: 'CCCB6QD9S',
+      channel_name: 'bot-cmd', // <- this eliminates the need for getChannelName
+      user_id: 'U1FF6KSF9',
+      user_name: 'woozyking',
+      command: '/diff',
+      text: 'snoke',
+      api_app_id: 'A6HPM5VC0',
+      is_enterprise_install: 'false',
+      response_url: 'https://hooks.slack.com/commands/T1FDR4NG3/2956432472786/J8Q17JPFukiLurI8zbVJzTCX',
+      trigger_id: '2956462218819.49467158547.0dae80ec6d595b045efcf7a8fee079e7'
+    }
+  */
+  app.command(`/${name}`, listener)
+
+  // interactive views, by callback_id
   if (viewHandler) {
     app.view(name,  viewHandler)
   }
